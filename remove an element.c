3afb0ce53655f971d68cc6ead969d4e0ba3d37d0
yazhuram2007bci29-hub <yazhuram2007_bci29@mepcoeng.ac.in>
2026-07-28 16:33:09 +0530

    Create remove an element.c

---
+++ remove an element.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0; 
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i]; 
+            k++;              
+        }
+    }
+    
+    return k; 
+}
